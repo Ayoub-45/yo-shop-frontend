7b667fc8df19cc37093275b15271453557da2e7d
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2024-09-17 23:30:39 +0100

    feat:Create productlist component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // Redirect to products by default
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.component').then((m) => m.ProductsComponent),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: 'cart',
+    loadChildren: () =>
+      import('./cart/cart.component').then((m) => m.CartComponent),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.component').then((m) => m.CheckoutComponent),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.component').then((m) => m.AdminComponent),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
